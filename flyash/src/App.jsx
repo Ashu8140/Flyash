9ad4fa68544339c40b126af200ca93e9d29e547a
Ashu <ashusainijijwania8140@gmail.com>
2025-03-21 19:28:20 +0530

    modify App

--- flyash/src/App.jsx
+++ flyash/src/App.jsx
@@ -1,7 +1,10 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold ">
+    <h1 className="text-3xl font-bold bg-red-600">
      Flyash briks
+    <h1>data
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis accusantium, unde rem iure reprehenderit id placeat facere delectus, deserunt veritatis voluptatem incidunt iusto non! Fugit dolor accusantium labore mollitia ex!</p>
+    </h1>
     </h1>
   )
 }
